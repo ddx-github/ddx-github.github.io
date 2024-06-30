39d95fbcea59fe12ae7a6ff04b42f9542028adaf
ddx-github <d201105092023@163.com>
2024-06-30 10:31:06 +0800

    Index

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ function check(thisform) {
 	var name=document.getElementById("name").value;
 	var pass=document.getElementById("pass").value;
 	if (name=="ddx" && pass=="ddx110509" || name=="xrh" && pass=="xrh740920") {
+		localStorage.setItem("user",name)
+		localStorage.setItem("pwd",pass)
 		alert("登录成功！");
-		window.document.f.action="test.html";
+		window.document.f.action="home.html";
 		window.document.f.submit();
 	}
 	else{
